e2474c6d3b29c847f6159ad2652c2da1fa0d7dc8
pragatiTendolkar <95703908+pragatiTendolkar@users.noreply.github.com>
2021-12-16 07:42:32 +0530

    Add files via upload

---
+++ program that prints the next 20 leap year.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Write a program that prints the next 20 leap year
+
+number = 4
+i=0
+while number % 4 == 0:
+    number = int(input(" Enter year "))
+    for i in range(20): 
+        if number % 4 == 0:
+            print(number)
+            number=number+4
+        else:
+            number=number+1
+        
+
+
+# In[ ]:
+
+
+
+
